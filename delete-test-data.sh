450f1baa8a06b8ea1313d36487fe4bc36925caf2
Tobias Kuhn <kuhntobias@gmail.com>
2019-03-06 12:31:00 +0100

    Properly clean data and use latest Virtuoso

--- delete-test-data.sh
+++ delete-test-data.sh
@@ -1,3 +1,3 @@
-sudo rm -rf ./test-data/mongodb/*
-sudo rm -rf ./test-data/rdfsink/*
-sudo rm -rf ./test-data/virtuoso/*
+sudo rm -rf ./test-data/mongodb
+sudo rm -rf ./test-data/rdfsink
+sudo rm -rf ./test-data/virtuoso
