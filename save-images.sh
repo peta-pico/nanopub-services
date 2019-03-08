8ac294829a7c338e57eceb8174c73140ca4b914a
Tobias Kuhn <kuhntobias@gmail.com>
2019-03-08 15:25:57 +0100

    Use images from nanopub/

---
+++ save-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p images
+
+docker save -o images/nanopub-mongo.tar nanopub/mongo
+docker save -o images/nanopub-server.tar nanopub/server
+docker save -o images/nanopub-rdfsink.tar nanopub/rdfsink
+docker save -o images/nanopub-virtuoso.tar nanopub/virtuoso
+docker save -o images/nanopub-grlc.tar nanopub/grlc
