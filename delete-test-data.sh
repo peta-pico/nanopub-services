324c3400536b8f0ed917989d9e3d037a06fcdd56
Tobias Kuhn <kuhntobias@gmail.com>
2019-03-08 15:09:02 +0100

    Work on docker-compose file

--- delete-test-data.sh
+++ delete-test-data.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# For testing environment, you can add a symbolic link from test-data to data:
+#
+# $ ln -s data test-data
+#
+# And then you can use this script the delete the test data and start afresh.
+
 sudo rm -rf ./test-data/mongodb
 sudo rm -rf ./test-data/rdfsink
 sudo rm -rf ./test-data/virtuoso
